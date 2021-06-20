654d5370f817b9259972720774a6f9669e4bd157
NotImplementedLife <not.implemented.life@gmail.com>
2021-06-20 09:56:53 +0300

    TilesetGen: added load option

--- misc/TilesetGenerator/main.js
+++ misc/TilesetGenerator/main.js
@@ -214,13 +214,13 @@ function encode() {
 		var tile=tiles[i];
 		for(var y=0;y<8;y++) {
 			var b0=0;
-			var b1=0;
+			var b1=0;			
 			for(x=0;x<8;x++) {
 				b0*=2; b1*=2;
 				var cl=tile[8*y+x];
 				if(cl==1 || cl==3) b0++;
 				if(cl==2 || cl==3) b1++;				
-			}
+			}			
 			str+= "$"+b0.toString(16).padStart(2,'0')+", $"+b1.toString(16).padStart(2,'0');
 			if(y==7) str+="\n";
 			else str+=", ";
@@ -228,6 +228,59 @@ function encode() {
 	}
 	document.getElementById("result").value=str;
 }
+
+function load() {
+	var k=0;
+	var str=document.getElementById("result").value;
+	var lines=str.match(/[^\r\n]+/g);
+	for(var i in lines) {
+		var line=lines[i];
+		var nb=line.replace(/[^0-9a-f]/g, "");
+		//console.log(nb);
+		nb=hex2bin(nb);
+		if(nb.length!=128) continue;		
+		for(var y=0;y<8;y++) {
+			for(var x=0;x<8;x++) {
+				var b0=nb[16*y+x]-'0';
+				var b1=nb[16*y+x+8]-'0';				
+				tiles[k][8*y+x]=b1*2+b0;
+			}			
+		}
+		updateVisualTM(k/8,k%8);
+		k++;		
+	}		
+	renderAllTiles();
+}
+
+//https://stackoverflow.com/questions/45053624/convert-hex-to-binary-in-javascript
+function hex2bin(hex){
+    hex = hex.replace("0x", "").toLowerCase();
+    var out = "";
+    for(var c of hex) {
+        switch(c) {
+            case '0': out += "0000"; break;
+            case '1': out += "0001"; break;
+            case '2': out += "0010"; break;
+            case '3': out += "0011"; break;
+            case '4': out += "0100"; break;
+            case '5': out += "0101"; break;
+            case '6': out += "0110"; break;
+            case '7': out += "0111"; break;
+            case '8': out += "1000"; break;
+            case '9': out += "1001"; break;
+            case 'a': out += "1010"; break;
+            case 'b': out += "1011"; break;
+            case 'c': out += "1100"; break;
+            case 'd': out += "1101"; break;
+            case 'e': out += "1110"; break;
+            case 'f': out += "1111"; break;
+            default: return "";
+        }
+    }
+
+    return out;
+}
+
 
 // prevent zoom:
 document.body.addEventListener("wheel", e=>{if(e.ctrlKey) e.preventDefault();}, { passive: false});
